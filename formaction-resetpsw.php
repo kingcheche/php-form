d5fe2f665c86d9efa78bfbb9c8340e90d0c817bc
Cheche Kalu <kalugreymatter@gmail.com>
2021-04-17 19:05:04 +0100

    PHP Reg/login form = No css

--- formaction-resetpsw.php
+++ formaction-resetpsw.php
@@ -1,3 +1,4 @@
+<?php session_start();?>
 <!DOCTYPE html>
 <html>
    <head>
@@ -8,23 +9,52 @@
 
       <body>
       
-      <div class="container">
+      <div class="container">      
       
       <?php 
+       
+       
+
       
        //Message if user is coming from reset password button
        if(isset($_POST["resetbtn"])) {
 
+
+        // Give error if confirmation password is not the same with new password
         if ($_POST["n_password"] != $_POST["nc_password"]) {
             echo "<h2> Error </h2>";
             echo "<hr>";
            echo "New password not confirmed, <a href='resetpsw.php'>Try again</a>";
            echo "<br>";
         } else {
-  
+
+            //Decode user file details
+            
+         $entry_data = file_get_contents("entry/" .$_SESSION["email"] .".json");
+         $user_data = json_decode($entry_data);
+   
+   //Convert data from user file to new variables
+   $name = $user_data->name;
+   $email = $user_data->email;
+   $password = $user_data->password;
+   //change password in file to new password
+   $password = $_POST["n_password"];
+
+   //create a new array and save the new data      
+   $new_data =  [
+            "name" => $user_data->name,
+            "email" => $user_data->email,
+            "password" => $_POST["n_password"]
+        ];
+
+        file_put_contents("entry/" .$_SESSION["email"] . ".json", json_encode($new_data));
+
+
+  //password change successful
             echo "<h2> Change successful </h2>";
             echo "<hr>";
            echo "<p class='message'>Login with new password <a href='login.php'>here</a></p>";
         }
         }
+
         ?>
